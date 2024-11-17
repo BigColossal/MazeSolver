92f659162183fd4ad676c100e508adcc6b56a956
BigColossal <jeremyperez1409@gmail.com>
2024-11-17 15:31:53 -0500

    Adjusted speed

--- maze.py
+++ maze.py
@@ -43,7 +43,7 @@ class Maze:
         if self._win is None:
             return
         self._win.redraw()
-        sleep(0.05)
+        sleep(0.02)
 
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
